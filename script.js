595ca4f566a6c24f44a6c7f97ac26897c950acd4
dinesh9123456 <150464507+dinesh9123456@users.noreply.github.com>
2025-05-31 19:58:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Select the button by its ID
+var button = document.getElementById("alertButton");
+// Add a click event listener to the button
+button.addEventListener("click", function() {
+    alert("Hello! You clicked the button.");
+});
+
